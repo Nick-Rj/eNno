8d27bd89c4de0c17f792052887d6a64f45c25128
Raj, Nikhil <nik.rj10348@gmail.com>
2021-06-12 22:37:35 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// var addRow = document.querySelectorAll(".add-row");
+// var removeRow = document.querySelectorAll(".remove-row");
+// var hideColumn = document.querySelectorAll(".hide-column");
+
+// var resizeWindow = () => {
+//   if (window.innerWidth <= 600) {
+//     addRow.classList.add("row");
+//     removeRow.classList.remove("row");
+//     hideColumn.style.display = "none";
+//   } else if (window.innerWidth > 600) {
+//     addRow.classList.remove("row");
+//     removeRow.classList.add("row");
+//     hideColumn.style.display = "block";
+//   }
+// };
+
+// window.onresize = () => {
+//   resizeWindow();
+// };
